2c8d1cd4e02cdfaa4443a7219c3e841273b0794b
Your Name <usmankhan>
2025-07-16 17:37:14 +0500

    Completed this fullstack app for now due to ending batch of Sir Yasir

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -5,9 +5,13 @@ import { Product } from "../model/products.model.js";
 export const productsHomePage = (req, res) => {
   res.send("Welcome to home page of products");
 };
-export const getAllProducts = async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
+export const getAllProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getSingleProduct = async (req, res) => {
@@ -21,10 +25,14 @@ export const updateProduct = async(req, res) => {
   const p = await Product.findByIdAndUpdate(id, body);
   res.json(p)
 };
-export const newProduct = async (req, res) => {
-  const newProduct = req.body;
-  const p = await Product.create(newProduct);
-  res.json(p);
+export const newProduct = async (req, res, next) => {
+  try {
+    const newProduct = req.body;
+    const p = await Product.create(newProduct);
+    res.json(p);
+  } catch (error) {
+    next(error);
+  }
 };
 export const deleteProduct = async(req, res) => {
   const { id } = req.params;
--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,10 @@ connectDB();
 app.use(bodyParser.json());
 app.use("/", router);
 
+app.use((err, req, res, next) => {
+  console.log(err);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
